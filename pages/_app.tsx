661dc490c8e6ca6d8a27463cc552ed5de7b3a466
theRealPadster <padspambox@gmail.com>
2021-09-18 21:56:11 -0400

    🚧 💄 Test adding dark mode toggle + theme, Add social icons footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import withDarkMode from 'next-dark-mode'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
-export default MyApp
+export default withDarkMode(MyApp);
